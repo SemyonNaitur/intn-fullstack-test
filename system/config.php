d0b476a48f54fc596a8d199e5a1ece106b7871e9
Semyon Naitur <SemyonNaitur@gmail.com>
2020-10-25 19:01:41 +0200

    switched to default autoloader

--- system/config.php
+++ system/config.php
@@ -10,29 +10,42 @@ define('PAGES_DIR', ROOT_DIR . '/pages');
 //--- /file structure ---//
 
 
+//--- app config ---//
 $app_config = [];
 function get_config(string $item = '')
 {
     global $app_config;
     return ($item) ? ($app_config[$item] ?? null) : $app_config;
 }
+//--- /app config ---//
 
 
-spl_autoload_register(function ($class) { // https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
-    $pfx = 'System\\';
-    $len = strlen($pfx);
-    if (strncmp($pfx, $class, $len) !== 0) return;
-    $cls = substr($class, $len);
-    $file = SYS_DIR . '/' . str_replace('\\', '/', $cls) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+//--- autoloader ---//
+define('CUSTOM_AUTOLOADER', 0);
+if (CUSTOM_AUTOLOADER) { // https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+    spl_autoload_register(function ($class) {
+        $pfx = 'System\\';
+        $len = strlen($pfx);
+        if (strncmp($pfx, $class, $len) !== 0) return;
+        $cls = substr($class, $len);
+        $file = SYS_DIR . '/' . str_replace('\\', '/', $cls) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    });
+} else { // https://www.php.net/manual/en/function.spl-autoload.php#92767
+    set_include_path(get_include_path() . PATH_SEPARATOR . SYS_DIR);
+    spl_autoload_extensions('.php');
+    spl_autoload_register();
+}
+//--- /autoloader ---//
 
 
+//--- error handling ---//
 set_error_handler(function ($severity, $message, $file, $line) {
     if (!(error_reporting() & $severity)) {
         return;
     }
     throw new ErrorException($message, 0, $severity, $file, $line);
 });
+//--- /error handling ---//
