b2e5106e666777c89cef86c5aa3419d3de8bdafa
Semyon Naitur <SemyonNaitur@gmail.com>
2021-01-22 12:44:07 +0200

    added methods to Curl

--- system/libraries/Curl.php
+++ system/libraries/Curl.php
@@ -8,14 +8,39 @@ class Curl
 
 	public function getContent(string $url): array
 	{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
+		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 		$result = curl_exec($ch);
 		$error = curl_error($ch);
 		curl_close($ch);
 
-		return ['result' => $result, 'error' => $error];
+		return compact('result', 'error');
+	}
+
+	public function getStatus(string $url): array
+	{
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		curl_exec($ch);
+		$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$error = curl_error($ch);
+		curl_close($ch);
+
+		return compact('status', 'error');
+	}
+
+	public function getContentType(string $url): array
+	{
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		curl_exec($ch);
+		$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+		$error = curl_error($ch);
+		curl_close($ch);
+
+		return compact('content_type', 'error');
 	}
 }
