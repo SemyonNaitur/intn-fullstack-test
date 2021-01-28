003e17119e63cde44b05f6211789814310665404
Semyon Naitur <SemyonNaitur@gmail.com>
2021-01-28 18:54:20 +0200

    added exception handling

--- system/core/App.php
+++ system/core/App.php
@@ -41,31 +41,46 @@ class App
 
     public function run()
     {
-        $preload = app_config('preload');
+        try {
+            $preload = app_config('preload');
 
-        if ($preload['db']) $this->loader->db();
+            if ($preload['db']) $this->loader->db();
 
-        ['route' => $route, 'params' => $params] = $this->router->matchUrl($this->request->uri());
+            ['route' => $route, 'params' => $params] = $this->router->matchUrl($this->request->uri());
 
-        if ($route) {
-            [$controller_path, $method] = explode('::', $route['method']);
+            if ($route) {
+                [$controller_path, $method] = explode('::', $route['method']);
 
-            $cls = Loader::checkSuffix('controller', $controller_path);
-            $cls = app_config('controllers_path') . '/' . $cls;
-            $cls = str_replace('/', '\\', $cls);
+                $cls = Loader::checkSuffix('controller', $controller_path);
+                $cls = app_config('controllers_path') . '/' . $cls;
+                $cls = str_replace('/', '\\', $cls);
 
-            $c = new $cls();
-            $c->$method($params, $route['data']);
-            $this->controller = $c;
-        } else {
-            http_response_code(404);
-            die('<h4>Page not found</h4>');
+                $c = new $cls();
+                $c->$method($params, $route['data']);
+                $this->controller = $c;
+            } else {
+                $this->errorResponse(404, 'Not Found');
+            }
+        } catch (\Throwable $e) {
+            $this->handleException($e);
+            $this->errorResponse(500, 'Internal Server Error');
         }
     }
 
     protected function getController(): Controller
     {
         return $this->controller;
+    }
+
+    protected function handleException(\Throwable $e)
+    {
+        $this->logger->error($e);
+    }
+
+    protected function errorResponse(int $code, string $message)
+    {
+        http_response_code($code);
+        die("<h2>$message</h2>");
     }
 
     public static function request(): Request
